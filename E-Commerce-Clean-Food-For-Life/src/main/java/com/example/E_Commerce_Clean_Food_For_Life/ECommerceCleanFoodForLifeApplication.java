e53761deddd57b5a233458221db90037c2b87990
AlexMound <caophong3107aa@gmail.com>
2024-10-07 16:44:54 +0700

    Initial commit on develop branch

---
+++ E-Commerce-Clean-Food-For-Life/src/main/java/com/example/E_Commerce_Clean_Food_For_Life/ECommerceCleanFoodForLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce_Clean_Food_For_Life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCleanFoodForLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCleanFoodForLifeApplication.class, args);
+	}
+
+}
---
+++ E-Commerce-Clean-Food-For-Life/src/test/java/com/example/E_Commerce_Clean_Food_For_Life/ECommerceCleanFoodForLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce_Clean_Food_For_Life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCleanFoodForLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
